4931a3db53c6c0a55c75b8cfaa44ae2d1cc5f09b
ycy190 <dsvalensky@yahoo.com>
2021-09-25 02:34:05 +1200

    MainPage updated, Navbar updated for mainpage and searchAction
    Updated user avatar, dropdown settings and re-designed its layout.

--- client/src/components/SearchAction.js
+++ client/src/components/SearchAction.js
@@ -46,27 +46,40 @@ class SearchAction extends Component{
         return (
             <><Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">SEPER</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/search">Search</Nav.Link>
-                        <Nav.Link href="#pricing">Upload</Nav.Link>
+                    <Navbar.Brand  href="/">SEPER</Navbar.Brand>
+                    <Nav className="mr-auto" >
+                    <Nav.Link  href="/">Home</Nav.Link>
+                    <Nav.Link  href="/search">Search</Nav.Link>
+                    <Nav.Link  href="/">Upload</Nav.Link>
                     </Nav>
+                    <div onClick={this.profileClick}>
+                        <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>   
+                    </div>
+                    <div>
+                        <Dropdown text='User name'>
+                            <Dropdown.Menu>
+                            <Dropdown.Item text='Browse profile' />
+                            <Dropdown.Item text='Edit profile' />
+                            <Dropdown.Item text='Logout' description='logout' />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        
+                    </div>
                 </Container>
-                </Navbar>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        search content:
-                        <textarea value={this.state.value} onChange={this.handleChange} />
-                        <select value={this.state.filter} onChange={this.handleFilterChange}>
-                            <option value="A">A</option>
-                            <option value="B">B</option>
-                            <option value="C">C</option>
-                            <option value="D">D</option>
-                        </select>
-                    </label>
-                    <input type="submit" value="submit" />
-                </form>
+            </Navbar>
+              <form onSubmit={this.handleSubmit}>
+                  <label>
+                      search content:
+                      <textarea value={this.state.value} onChange={this.handleChange} />
+                      <select value={this.state.filter} onChange={this.handleFilterChange}>
+                          <option value="A">A</option>
+                          <option value="B">B</option>
+                          <option value="C">C</option>
+                          <option value="D">D</option>
+                      </select>
+                  </label>
+                  <input type="submit" value="submit" />
+              </form>
             </>
           
         );
